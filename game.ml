f7019d8c930daf2d914ad3c2e654683c60dc6061
Zyad <100876896+ZyadNzoulou@users.noreply.github.com>
2023-10-06 10:40:14 +0200

    Fixed a mistake and added very simple simulation

--- game.ml
+++ game.ml
@@ -29,7 +29,7 @@ let go_to_jail pl =
 let move (pl:player) (moves:int)=
   (*On vérifie si le joueur est en prison*)
   if pl.in_jail then () else  
-  pl.pos <- pl.pos + moves;
+  pl.pos <- (pl.pos + moves) mod 39;
   (*On vérifie si on veut envoyer le joueur en prison et on vérifie si il est dans une case allez en prison*)
   if (pl.pos = 30) then go_to_jail pl
 
@@ -89,25 +89,32 @@ let (properties: case array) = [|
   {id = 20; name = "Avenue Matignon"; c_type = Rouge; price = 220; rent = 18; isAvailable = true; ownedBy = None };
   {id = 21; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
   {id = 22; name = "Boulevard Malsherbes"; c_type = Rouge; price = 220; rent = 18; isAvailable = true; ownedBy = None };
-  {id = 23; name = "Gare du Nord"; c_type = Rouge; price = 200; rent = 1; isAvailable = true; ownedBy = None };
-  {id = 24; name = "Faubourg Saint-Honoré"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
-  {id = 25; name = "Place de la Bourse"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
-  {id = 26; name = "Compagnie de Distribution des Eaux"; c_type = ServPub; price = 150; rent = 1; isAvailable = true; ownedBy = None };
-  {id = 27; name = "Rue La Fayette"; c_type = Jaune; price = 280; rent = 24; isAvailable = true; ownedBy = None };
-  {id = 28; name = "Allez en Prison"; c_type = GoJail; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 29; name = "Avenue de Breteuil"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 29; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 29; name = "Caisse de Communauté"; c_type = Commu; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 29; name = "Boulevard des Capucines"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 29; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 29; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 29; name = "Avenue des Champs-Elysées"; c_type = Bleu; price = 350; rent = 35; isAvailable = true; ownedBy = None };
-  {id = 29; name = "Taxe de Luxe"; c_type = Impot; price = 0; rent = 100; isAvailable = false; ownedBy = None };
-  {id = 29; name = "Rue de la Paix"; c_type = Bleu; price = 400; rent = 50; isAvailable = true; ownedBy = None };
+  {id = 23; name= "Avenue Henri-Martin"; c_type = Rouge; price = 240; rent = 20 ; isAvailable = true; ownedBy = None};
+  {id = 24; name = "Gare du Nord"; c_type = Rouge; price = 200; rent = 1; isAvailable = true; ownedBy = None };
+  {id = 25; name = "Faubourg Saint-Honoré"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
+  {id = 26; name = "Place de la Bourse"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
+  {id = 27; name = "Compagnie de Distribution des Eaux"; c_type = ServPub; price = 150; rent = 1; isAvailable = true; ownedBy = None };
+  {id = 28; name = "Rue La Fayette"; c_type = Jaune; price = 280; rent = 24; isAvailable = true; ownedBy = None };
+  {id = 29; name = "Allez en Prison"; c_type = GoJail; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 30; name = "Avenue de Breteuil"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 31; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 32; name = "Caisse de Communauté"; c_type = Commu; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 33; name = "Boulevard des Capucines"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 34; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 35; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 36; name = "Avenue des Champs-Elysées"; c_type = Bleu; price = 350; rent = 35; isAvailable = true; ownedBy = None };
+  {id = 37; name = "Taxe de Luxe"; c_type = Impot; price = 0; rent = 100; isAvailable = false; ownedBy = None };
+  {id = 38; name = "Rue de la Paix"; c_type = Bleu; price = 400; rent = 50; isAvailable = true; ownedBy = None };
 
 |]
-let create_board () = let board = Hashtbl.create 40 in
-    for i=0 to 39 do 
-      Hashtbl.add board i (properties.(i))
-    done;
-    board
+
+
+(*Partie simulation*)
+let test () =
+  let (player1:player) = {id = 0; risky = 0.0; pos = 0; in_jail = false; money = 1500; properties = (Array.make 39 false); turns_injail = 0} in
+  for i = 0 to 50 do 
+    player_dr player1;
+    if player1.in_jail then Printf.printf "Liberez tous mes copains \n"
+  done;
+  Printf.printf "La position actuelle du joueur est: %d" player1.pos  
+
