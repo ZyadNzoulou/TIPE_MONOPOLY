236d39f5de7d05e6f7aa1b9e5df6f4ea4ac099e8
Zyad <100876896+ZyadNzoulou@users.noreply.github.com>
2023-10-23 19:00:51 +0200

    Fixed properties ID list

--- game.ml
+++ game.ml
@@ -34,7 +34,7 @@ let move (pl:player) (moves:int)=
   if pl.in_jail && pl.turns_injail < 3 then () else
   pl.pos <- (pl.pos + moves) mod 39;
   (*On vérifie si on veut envoyer le joueur en prison et on vérifie si il est dans une case allez en prison*)
-  if (pl.pos = 29) then go_to_jail pl
+  if (pl.pos = 30) then go_to_jail pl
 
 
 (*Jette un dè et renvoie sa valeur*)
@@ -75,42 +75,43 @@ let (properties: case array) = [|
   {id = 0; name = "Départ"; c_type = Go; price = 0; rent = 0; isAvailable = false; ownedBy = None };
   {id = 1; name = "Boulevard de Belleville"; c_type = Marron; price = 60; rent = 2; isAvailable = true; ownedBy = None };
   {id = 2; name = "Caisse de Communauté"; c_type = Commu; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 3; name = "Impôts sur le Revenu"; c_type = Impot; price = 0; rent = 200; isAvailable = false; ownedBy = None };
-  {id = 4; name = "Gare Montparnasse"; c_type = Gare; price = 200; rent = 25; isAvailable = true; ownedBy = None };
-  {id = 5; name = "Rue de Vaugirard"; c_type = Bleu_Ciel; price = 100; rent = 6; isAvailable = true; ownedBy = None };
-  {id = 6; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 7; name = "Rue de Courcelles"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 8; name = "Avenue de la République"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 9; name = "Prison"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 10; name = "Boulevard de la Villette"; c_type = Rose; price = 140; rent = 10; isAvailable = true; ownedBy = None };
-  {id = 11; name = "Compagnie de Distribution d'Électricité"; c_type = ServPub; price = 150; rent = 1; isAvailable = true; ownedBy = None };
-  {id = 12; name = "Avenue de Neuilly"; c_type = Rose; price = 140; rent = 10; isAvailable = true; ownedBy = None };
-  {id = 13; name = "Rue de Paradis"; c_type = Rose; price = 160; rent = 12; isAvailable = true; ownedBy = None };
-  {id = 14; name = "Gare de Lyon"; c_type = Gare; price = 200; rent = 25; isAvailable = true; ownedBy = None };
-  {id = 15; name = "Avenue Mozart"; c_type = Orange; price = 180; rent = 14; isAvailable = true; ownedBy = None };
-  {id = 16; name = "Caisse de Communauté"; c_type = Commu; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 17; name = "Boulevard Saint Michel"; c_type = Orange; price = 180; rent = 14; isAvailable = true; ownedBy = None };
-  {id = 18; name = "Place Pigalle"; c_type = Orange; price = 200; rent = 16; isAvailable = true; ownedBy = None };
-  {id = 19; name = "Parc Gratuit"; c_type = Park; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 20; name = "Avenue Matignon"; c_type = Rouge; price = 220; rent = 18; isAvailable = true; ownedBy = None };
-  {id = 21; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 22; name = "Boulevard Malsherbes"; c_type = Rouge; price = 220; rent = 18; isAvailable = true; ownedBy = None };
-  {id = 23; name= "Avenue Henri-Martin"; c_type = Rouge; price = 240; rent = 20 ; isAvailable = true; ownedBy = None};
-  {id = 24; name = "Gare du Nord"; c_type = Rouge; price = 200; rent = 1; isAvailable = true; ownedBy = None };
-  {id = 25; name = "Faubourg Saint-Honoré"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
-  {id = 26; name = "Place de la Bourse"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
-  {id = 27; name = "Compagnie de Distribution des Eaux"; c_type = ServPub; price = 150; rent = 1; isAvailable = true; ownedBy = None };
-  {id = 28; name = "Rue La Fayette"; c_type = Jaune; price = 280; rent = 24; isAvailable = true; ownedBy = None };
-  {id = 29; name = "Allez en Prison"; c_type = GoJail; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 30; name = "Avenue de Breteuil"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 31; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 32; name = "Caisse de Communauté"; c_type = Commu; price = 300; rent = 26; isAvailable = false; ownedBy = None };
-  {id = 33; name = "Boulevard des Capucines"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 34; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
-  {id = 35; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
-  {id = 36; name = "Avenue des Champs-Elysées"; c_type = Bleu; price = 350; rent = 35; isAvailable = true; ownedBy = None };
-  {id = 37; name = "Taxe de Luxe"; c_type = Impot; price = 0; rent = 100; isAvailable = false; ownedBy = None };
-  {id = 38; name = "Rue de la Paix"; c_type = Bleu; price = 400; rent = 50; isAvailable = true; ownedBy = None };
+  {id = 3; name = "Rue Lecourbe"; c_type = Marron; price = 60; rent = 4; isAvailable = true; ownedBy = None };
+  {id = 4; name = "Impôts sur le Revenu"; c_type = Impot; price = 0; rent = 200; isAvailable = false; ownedBy = None };
+  {id = 5; name = "Gare Montparnasse"; c_type = Gare; price = 200; rent = 25; isAvailable = true; ownedBy = None };
+  {id = 6; name = "Rue de Vaugirard"; c_type = Bleu_Ciel; price = 100; rent = 6; isAvailable = true; ownedBy = None };
+  {id = 7; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 8; name = "Rue de Courcelles"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 9; name = "Avenue de la République"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 10; name = "Prison"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 11; name = "Boulevard de la Villette"; c_type = Rose; price = 140; rent = 10; isAvailable = true; ownedBy = None };
+  {id = 12; name = "Compagnie de Distribution d'Électricité"; c_type = ServPub; price = 150; rent = 1; isAvailable = true; ownedBy = None };
+  {id = 13; name = "Avenue de Neuilly"; c_type = Rose; price = 140; rent = 10; isAvailable = true; ownedBy = None };
+  {id = 14; name = "Rue de Paradis"; c_type = Rose; price = 160; rent = 12; isAvailable = true; ownedBy = None };
+  {id = 15; name = "Gare de Lyon"; c_type = Gare; price = 200; rent = 25; isAvailable = true; ownedBy = None };
+  {id = 16; name = "Avenue Mozart"; c_type = Orange; price = 180; rent = 14; isAvailable = true; ownedBy = None };
+  {id = 17; name = "Caisse de Communauté"; c_type = Commu; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 18; name = "Boulevard Saint Michel"; c_type = Orange; price = 180; rent = 14; isAvailable = true; ownedBy = None };
+  {id = 19; name = "Place Pigalle"; c_type = Orange; price = 200; rent = 16; isAvailable = true; ownedBy = None };
+  {id = 20; name = "Parc Gratuit"; c_type = Park; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 21; name = "Avenue Matignon"; c_type = Rouge; price = 220; rent = 18; isAvailable = true; ownedBy = None };
+  {id = 22; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 23; name = "Boulevard Malsherbes"; c_type = Rouge; price = 220; rent = 18; isAvailable = true; ownedBy = None };
+  {id = 24; name= "Avenue Henri-Martin"; c_type = Rouge; price = 240; rent = 20 ; isAvailable = true; ownedBy = None};
+  {id = 25; name = "Gare du Nord"; c_type = Rouge; price = 200; rent = 1; isAvailable = true; ownedBy = None };
+  {id = 26; name = "Faubourg Saint-Honoré"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
+  {id = 27; name = "Place de la Bourse"; c_type = Jaune; price = 260; rent = 22; isAvailable = true; ownedBy = None };
+  {id = 28; name = "Compagnie de Distribution des Eaux"; c_type = ServPub; price = 150; rent = 1; isAvailable = true; ownedBy = None };
+  {id = 29; name = "Rue La Fayette"; c_type = Jaune; price = 280; rent = 24; isAvailable = true; ownedBy = None };
+  {id = 30; name = "Allez en Prison"; c_type = GoJail; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 31; name = "Avenue de Breteuil"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 32; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 33; name = "Caisse de Communauté"; c_type = Commu; price = 300; rent = 26; isAvailable = false; ownedBy = None };
+  {id = 34; name = "Boulevard des Capucines"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 35; name = "Avenue Foch"; c_type = Vert; price = 300; rent = 26; isAvailable = true; ownedBy = None };
+  {id = 36; name = "Chance"; c_type = Chance; price = 0; rent = 0; isAvailable = false; ownedBy = None };
+  {id = 37; name = "Avenue des Champs-Elysées"; c_type = Bleu; price = 350; rent = 35; isAvailable = true; ownedBy = None };
+  {id = 38; name = "Taxe de Luxe"; c_type = Impot; price = 0; rent = 100; isAvailable = false; ownedBy = None };
+  {id = 39; name = "Rue de la Paix"; c_type = Bleu; price = 400; rent = 50; isAvailable = true; ownedBy = None };
 
 |]
 
